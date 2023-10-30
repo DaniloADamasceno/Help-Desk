86eb9d14e4d9068cf9a5a83142e6a4f9356af846
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2023-10-30 13:52:01 -0300

    :alien: Criação de Rotas iniciais de navegação

--- Front End - Angular/helpdeskfront/src/app/app-routing.module.ts
+++ Front End - Angular/helpdeskfront/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavgationComponent } from './componets/navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NavgationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front End - Angular/helpdeskfront/src/app/app.module.ts
+++ Front End - Angular/helpdeskfront/src/app/app.module.ts
@@ -5,14 +5,65 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Realizar requisições HTTP
+import { HttpClientModule } from '@angular/common/http';
+
+// Formulários no Angular 12
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+
+// Angular Material IMPORTS
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+
+// Componentes do projeto
+import { NavgationComponent as NavigationComponent } from './componets/navigation/navigation.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    // Formularios
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Requisições http
+    HttpClientModule,
+
+    // Angular Material
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    MatRadioModule,
+    MatTableModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
