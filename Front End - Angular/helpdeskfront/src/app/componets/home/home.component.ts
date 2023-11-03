97792f61381e532b5d32a4aebc9c250dfcd41344
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2023-11-03 10:36:51 -0300

    :ambulance: Add a Branch Master

--- Front End - Angular/helpdeskfront/src/app/componets/home/home.component.ts
+++ Front End - Angular/helpdeskfront/src/app/componets/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: 'app-home',                       // O nome do seletor do componente  HTML
+  templateUrl: './home.component.html',       // O arquivo HTML
+  styleUrls: ['./home.component.css']         // O arquivo CSS
 })
 export class HomeComponent implements OnInit {
 
--- Front End - Angular/helpdeskfront/src/app/componets/navigation/navigation.component.ts
+++ Front End - Angular/helpdeskfront/src/app/componets/navigation/navigation.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Router } from '@angular/router';
 
-
 @Component({
-  selector: 'app-navigation',                       // O nome do seletor do componente  HTML       
+  selector: 'app-navigation',                       // O nome do seletor do componente  HTML
   templateUrl: './navigation.component.html',       // O arquivo HTML
   styleUrls: ['./navigation.component.css']         // O arquivo CSS
 })
-export class NavgationComponent implements OnInit {
-
+export class NavigationComponent implements OnInit {
 
   constructor(private router: Router) { }
 
@@ -18,5 +15,4 @@ export class NavgationComponent implements OnInit {
     this.router.navigate(['/home']);                // Navegação para a página home
 
   }
-
 }
